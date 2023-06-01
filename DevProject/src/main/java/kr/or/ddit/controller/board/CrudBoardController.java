369f098d3dfc5512b419e2409a1905366607a717
GuGihyeon <ggh4177@gmail.com>
2023-06-01 18:30:00 +0900

    CrudBoardController

--- DevProject/src/main/java/kr/or/ddit/controller/board/CrudBoardController.java
+++ DevProject/src/main/java/kr/or/ddit/controller/board/CrudBoardController.java
@@ -1,5 +1,7 @@
 package kr.or.ddit.controller.board;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
@@ -39,6 +41,85 @@ public class CrudBoardController {
 		return "crud/success";
 	}
 	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String crudList(Model model) {
+		log.info("crudList() 실행...!");
+		
+		List<Board> boardList = service.list();
+		model.addAttribute("boardList", boardList);
+		
+		return "crud/list";
+	}
 	
+	@RequestMapping(value = "/read", method = RequestMethod.GET)
+	public String crudRead(Model model, int boardNo) {
+		log.info("crudRead() 실행...!");
+		
+		Board board = service.read(boardNo);
+		log.info("board : " + board);
+		model.addAttribute("board", board);
+		
+		return "crud/read";
+	}
 	
+	@RequestMapping(value = "/modify", method = RequestMethod.GET)
+	public String crudModifyForm(Model model, int boardNo) {
+		log.info("crudModifyForm() 실행...!");
+		
+		Board board = service.read(boardNo);
+		model.addAttribute("board", board);
+		model.addAttribute("status", "u");		// '수정을 진행중입니다' 라는 flag 값
+		
+		return "crud/register";
+	}
+	
+	@RequestMapping(value = "/modify", method = RequestMethod.POST)
+	public String crudModify(Model model, Board board) {
+		log.info("crudModify() 실행...!");
+		
+		service.update(board);
+		model.addAttribute("msg", "수정이 완료되었습니다.");
+		
+		return "crud/success";
+	}
+	
+	@RequestMapping(value = "/remove", method = RequestMethod.POST)
+	public String crudDelete(Model model, int boardNo) {
+		log.info("crudDelete() 실행...!");
+		
+		service.delete(boardNo);
+		model.addAttribute("msg", "삭제가 완료되었습니다.");
+		
+		return "crud/success";
+	}
+	
+	@RequestMapping(value = "/search", method = RequestMethod.POST)
+	public String crudSearch(Model model, String title) {
+		log.info("crudSearch() 실행...!");
+		
+		Board board = new Board();
+		board.setTitle(title);
+		
+		List<Board> boardList = service.search(board);
+		model.addAttribute("board", board);
+		model.addAttribute("boardList", boardList);
+		
+		return "crud/list";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
