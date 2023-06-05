335885b8b91a98b8d5363fe4b17cda10424f44d5
GuGihyeon <ggh4177@gmail.com>
2023-06-05 15:58:14 +0900

    ItemService interface

--- DevProject/src/main/java/kr/or/ddit/controller/file/item01/service/ItemService.java
+++ DevProject/src/main/java/kr/or/ddit/controller/file/item01/service/ItemService.java
@@ -1,9 +1,17 @@
 package kr.or.ddit.controller.file.item01.service;
 
+import java.util.List;
+
 import kr.or.ddit.vo.Item;
 
 public interface ItemService {
 
 	public void register(Item item);
+
+	public List<Item> list();
+
+	public Item read(int itemId);
+
+	public String getPicture(int itemId);
 
 }
