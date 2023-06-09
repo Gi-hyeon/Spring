69ff4caa6458969ffd4bf3c7af535b66b43d46a8
GuGihyeon <ggh4177@gmail.com>
2023-06-09 17:22:43 +0900

    NoticeModifyController, 수정 삭제 처리

--- DevProject/src/main/java/kr/or/ddit/controller/noticeboard/web/NoticeModifyController.java
+++ DevProject/src/main/java/kr/or/ddit/controller/noticeboard/web/NoticeModifyController.java
@@ -1,6 +1,7 @@
 package kr.or.ddit.controller.noticeboard.web;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -32,11 +33,13 @@ public class NoticeModifyController {
 	}
 	
 	@RequestMapping(value = "/update.do", method = RequestMethod.POST)
-	public String noticeModify(NoticeVO noticeVO, Model model) {
+	public String noticeModify(
+			HttpServletRequest req,
+			NoticeVO noticeVO, Model model) {
 		log.info("noticeModify() 실행...!");
 		String goPage = "";
 		
-		ServiceResult result = noticeService.updateNotice(noticeVO);
+		ServiceResult result = noticeService.updateNotice(req, noticeVO);
 		if (result.equals(ServiceResult.OK)) {
 			goPage = "redirect:/notice/detail.do?boNo=" + noticeVO.getBoNo();
 		} else {
@@ -49,11 +52,13 @@ public class NoticeModifyController {
 	}
 	
 	@RequestMapping(value = "/delete.do", method = RequestMethod.POST)
-	public String noticeDelete(int boNo, Model model) {
+	public String noticeDelete(
+			HttpServletRequest req,
+			int boNo, Model model) {
 		String goPage = "";
 		ServiceResult result = null;
 		
-		result = noticeService.deleteNotice(boNo);
+		result = noticeService.deleteNotice(req, boNo);
 		if (result.equals(ServiceResult.OK)) {
 			goPage = "redirect:/notice/list.do";
 		} else {
