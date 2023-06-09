c7dc3d878d4d12c3e975ee09a082cea351d86880
GuGihyeon <ggh4177@gmail.com>
2023-06-09 12:47:14 +0900

    NoticeInsertController, session값 불러와 작성자 다시 세팅해줌

--- DevProject/src/main/java/kr/or/ddit/controller/noticeboard/web/NoticeInsertController.java
+++ DevProject/src/main/java/kr/or/ddit/controller/noticeboard/web/NoticeInsertController.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
@@ -16,9 +18,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kr.or.ddit.ServiceResult;
 import kr.or.ddit.controller.noticeboard.service.INoticeService;
+import kr.or.ddit.vo.DDITMemberVO;
+import kr.or.ddit.vo.MemberVO;
 import kr.or.ddit.vo.NoticeVO;
 import lombok.extern.slf4j.Slf4j;
 
@@ -36,7 +41,10 @@ public class NoticeInsertController {
 	}
 	
 	@RequestMapping(value = "/insert.do", method = RequestMethod.POST)
-	public String noticeInsert(NoticeVO noticeVO, Model model) {
+	public String noticeInsert(
+			HttpServletRequest req,
+			RedirectAttributes ra,
+			NoticeVO noticeVO, Model model) {
 		String goPage = "";
 		Map<String, String> errors = new HashMap<String, String>();
 		if (StringUtils.isBlank(noticeVO.getBoTitle())) {
@@ -51,13 +59,21 @@ public class NoticeInsertController {
 			model.addAttribute("noticeVO", noticeVO);
 			goPage = "notice/form";
 		} else {
-			noticeVO.setBoWriter("a001");	// 임시로 넣어둔다. (로그인 처리 후 재 세팅할 예정)
-			ServiceResult result = noticeService.insertNotice(noticeVO);
-			if (result.equals(ServiceResult.OK)) {
-				goPage = "redirect:/notice/detail.do?boNo=" + noticeVO.getBoNo();
+			//noticeVO.setBoWriter("a001");	// 임시로 넣어둔다. (로그인 처리 후 재 세팅할 예정)
+			HttpSession session = req.getSession();
+			DDITMemberVO memberVO = (DDITMemberVO)session.getAttribute("SessionInfo");
+			if (memberVO != null) {
+				noticeVO.setBoWriter(memberVO.getMemId());
+				ServiceResult result = noticeService.insertNotice(req, noticeVO);
+				if (result.equals(ServiceResult.OK)) {
+					goPage = "redirect:/notice/detail.do?boNo=" + noticeVO.getBoNo();
+				} else {
+					model.addAttribute("message", "서버 에러, 다시 시도해주세요!");
+					goPage = "notice/form";
+				}
 			} else {
-				model.addAttribute("message", "서버 에러, 다시 시도해주세요!");
-				goPage = "notice/form";
+				ra.addFlashAttribute("message", "로그인 후에 사용가능합니다!");
+				goPage = "redirect:/notice/login.do";
 			}
 		}
 		
@@ -80,53 +96,53 @@ public class NoticeInsertController {
 	 * @ResponseBody : json, but.. 텍스트로 리턴해줌
 	 */
 	
-	@ResponseBody
-	@PostMapping("/generalFormPost")
-	public String generalFormPost(NoticeVO noticeVO) {
-		/*
-		 * NoticeVO(boNo=0, boTitle=title test, boContent=<p>content&nbsp;test</p>
-		 * , boWriter=writer test, boDate=null, boHit=0, delNoticeNo=null, 
-		 * boFile=[org.springframework.web.multipart.support.StandardMultipartHttpServletRequest$StandardMultipartFile@7a8d146e])
-		 */
-		log.info("noticeVO : " + noticeVO);
-		
-		// 파일을 업로드할 대상
-		String uploadFolder = "D:\\A_TeachingMaterial\\6_JspSpring\\02.SPRING2\\workspace_spring2\\DevProject\\src\\main\\webapp\\resources\\upload";
-		
-		// 파일 객체 꺼내오기
-		MultipartFile[] boFile = noticeVO.getBoFile();
-		
-		// 파일배열객체로부터 파일을 하나씩 꺼내오자
-		for(MultipartFile multipartFile : boFile) {
-			log.info("--------------------");
-			log.info("upload file name : " + multipartFile.getOriginalFilename());
-			log.info("upload file size : " + multipartFile.getSize());
-			log.info("upload file contentType : " + multipartFile.getContentType());	//MIME 타입
-			
-			// File 객체 복사 설계(복사할 대상 경로, 파일명)
-			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
-			
-			// 년/월/일 폴더 생성
-			// UUID 처리(파일명 중복 방지)
-			
-			// 파일 복사 실행(파일원본.transferTo(설계))
-			try {
-				multipartFile.transferTo(saveFile);
-				
-				// 썸네일 생성
-				return "Success";
-			} catch (IllegalStateException | IOException e) {
-				//e.printStackTrace();
-				log.error(e.getMessage());
-				return "Failed";
-			}
-		}
-		
-		// insert/update/delete의  경우 returnType = "int"를 생략
-		noticeService.insertNotice(noticeVO);
-		
-		return "Success";
-	}
+//	@ResponseBody
+//	@PostMapping("/generalFormPost")
+//	public String generalFormPost(NoticeVO noticeVO) {
+//		/*
+//		 * NoticeVO(boNo=0, boTitle=title test, boContent=<p>content&nbsp;test</p>
+//		 * , boWriter=writer test, boDate=null, boHit=0, delNoticeNo=null, 
+//		 * boFile=[org.springframework.web.multipart.support.StandardMultipartHttpServletRequest$StandardMultipartFile@7a8d146e])
+//		 */
+//		log.info("noticeVO : " + noticeVO);
+//		
+//		// 파일을 업로드할 대상
+//		String uploadFolder = "D:\\A_TeachingMaterial\\6_JspSpring\\02.SPRING2\\workspace_spring2\\DevProject\\src\\main\\webapp\\resources\\upload";
+//		
+//		// 파일 객체 꺼내오기
+//		MultipartFile[] boFile = noticeVO.getBoFile();
+//		
+//		// 파일배열객체로부터 파일을 하나씩 꺼내오자
+//		for(MultipartFile multipartFile : boFile) {
+//			log.info("--------------------");
+//			log.info("upload file name : " + multipartFile.getOriginalFilename());
+//			log.info("upload file size : " + multipartFile.getSize());
+//			log.info("upload file contentType : " + multipartFile.getContentType());	//MIME 타입
+//			
+//			// File 객체 복사 설계(복사할 대상 경로, 파일명)
+//			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+//			
+//			// 년/월/일 폴더 생성
+//			// UUID 처리(파일명 중복 방지)
+//			
+//			// 파일 복사 실행(파일원본.transferTo(설계))
+//			try {
+//				multipartFile.transferTo(saveFile);
+//				
+//				// 썸네일 생성
+//				return "Success";
+//			} catch (IllegalStateException | IOException e) {
+//				//e.printStackTrace();
+//				log.error(e.getMessage());
+//				return "Failed";
+//			}
+//		}
+//		
+//		// insert/update/delete의  경우 returnType = "int"를 생략
+//		noticeService.insertNotice(noticeVO);
+//		
+//		return "Success";
+//	}
 }
 
 
