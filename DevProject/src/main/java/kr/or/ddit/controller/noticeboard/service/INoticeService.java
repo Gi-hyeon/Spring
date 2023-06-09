9b8459da4d4d3b342719f471238358ac02b4c15b
GuGihyeon <ggh4177@gmail.com>
2023-06-09 17:20:22 +0900

    INoticeService interface, HttpServletRequest 추가, 등록 파일 다운, 프로필 업데이트 추가함

--- DevProject/src/main/java/kr/or/ddit/controller/noticeboard/service/INoticeService.java
+++ DevProject/src/main/java/kr/or/ddit/controller/noticeboard/service/INoticeService.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import kr.or.ddit.ServiceResult;
 import kr.or.ddit.vo.DDITMemberVO;
+import kr.or.ddit.vo.NoticeFileVO;
 import kr.or.ddit.vo.NoticeVO;
 import kr.or.ddit.vo.PaginationInfoVO;
 
@@ -15,9 +16,9 @@ public interface INoticeService {
 
 	public NoticeVO selectNotice(int boNo);
 
-	public ServiceResult updateNotice(NoticeVO noticeVO);
+	public ServiceResult updateNotice(HttpServletRequest seq, NoticeVO noticeVO);
 
-	public ServiceResult deleteNotice(int boNo);
+	public ServiceResult deleteNotice(HttpServletRequest req, int boNo);
 
 	public int selectNoticeCount(PaginationInfoVO<NoticeVO> pagingVO);
 
@@ -32,5 +33,11 @@ public interface INoticeService {
 	public String idForgetProcess(DDITMemberVO member);
 
 	public String pwForgetProcess(DDITMemberVO member);
+
+	public NoticeFileVO noticeDownload(int fileNo);
+
+	public DDITMemberVO selectMember(DDITMemberVO sessionMember);
+
+	public ServiceResult profileUpdate(HttpServletRequest req, DDITMemberVO memberVO);
 
 }
