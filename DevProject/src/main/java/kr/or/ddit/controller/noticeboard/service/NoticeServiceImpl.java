ba12f1f1e804a10c73f0b777ec9422616f07e0bc
GuGihyeon <ggh4177@gmail.com>
2023-06-09 17:21:31 +0900

    NoticeServiceImpl 게시글 삭제(이미지 삭제도 처리), 다운로드 추가(횟수도 증가함)

--- DevProject/src/main/java/kr/or/ddit/controller/noticeboard/service/NoticeServiceImpl.java
+++ DevProject/src/main/java/kr/or/ddit/controller/noticeboard/service/NoticeServiceImpl.java
@@ -15,6 +15,7 @@ import kr.or.ddit.ServiceResult;
 import kr.or.ddit.controller.noticeboard.web.TelegramSendController;
 import kr.or.ddit.mapper.LoginMapper;
 import kr.or.ddit.mapper.NoticeMapper;
+import kr.or.ddit.mapper.ProfileMapper;
 import kr.or.ddit.vo.DDITMemberVO;
 import kr.or.ddit.vo.NoticeFileVO;
 import kr.or.ddit.vo.NoticeVO;
@@ -28,6 +29,9 @@ public class NoticeServiceImpl implements INoticeService {
 	
 	@Inject
 	private LoginMapper loginMapper;
+	
+	@Inject
+	private ProfileMapper profileMapper;
 	
 	TelegramSendController tst = new TelegramSendController();
 	
@@ -66,11 +70,32 @@ public class NoticeServiceImpl implements INoticeService {
 	}
 
 	@Override
-	public ServiceResult updateNotice(NoticeVO noticeVO) {
+	public ServiceResult updateNotice(HttpServletRequest req, NoticeVO noticeVO) {
 		ServiceResult result = null;
 		
 		int status = noticeMapper.updateNotice(noticeVO);
-		if (status > 0) {
+		if (status > 0) {	// 성공
+			List<NoticeFileVO> noticeFileList = noticeVO.getNoticeFileList();
+			try {
+				// 1) 수정을 위해서 새로 추가된 파일 데이터를 먼저 업로드 처리한다.
+				noticeFileUpload(noticeFileList, noticeVO.getBoNo(), req);
+				// 2) 기존 추가되었던 파일들 중, 삭제를 원하는 파일 번호들을 가져온다.
+				Integer[] delNoticeNo = noticeVO.getDelNoticeNo();
+				if (delNoticeNo != null) {
+					// 3) 삭제를 원하는 파일 번호들 하나하나씩을 데이터베이스로 던져서 파일 정보를 얻어 온 다음에
+					// 데이터베이스에서 데이터를 삭제하고 서버 업로드 겨올에 업로드 되어있는 파일 데이터를 삭제한다.
+					for(int i=0; i<delNoticeNo.length; i++) {
+						NoticeFileVO noticeFileVO = noticeMapper.selectNoticeFile(delNoticeNo[i]);
+						noticeMapper.deleteNoticeFile(delNoticeNo[i]);
+						File file = new File(noticeFileVO.getFileSavepath());
+						file.delete();
+					}
+				}
+				
+			} catch (IllegalStateException | IOException e) {
+				e.printStackTrace();
+			}
+			
 			result = ServiceResult.OK;
 		} else {
 			result = ServiceResult.FAILED;
@@ -80,15 +105,54 @@ public class NoticeServiceImpl implements INoticeService {
 	}
 
 	@Override
-	public ServiceResult deleteNotice(int boNo) {
+	public ServiceResult deleteNotice(HttpServletRequest req, int boNo) {
 		ServiceResult result = null;
+		
+		// 본체인 notice를 삭제하기 전에 File 데이터를 먼저 삭제하기 위한 요청
+		noticeMapper.deleteNoticeFileByBoNo(boNo);
+		
+		// 아래에서 boNo에 해당하는 파일 데이터를 삭제할 때 사용할 객체(noticeVO)
+		NoticeVO noticeVO = noticeMapper.selectNotice(boNo);
+		
+		
 		int status = noticeMapper.deleteNotice(boNo);
 		if (status > 0) {
+			//D:\A_TeachingMaterial\6_JspSpring\02.SPRING2\workspace_spring2\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\wtpwebapps\DevProject\resources\notice\25/2a087772-f261-4b59-ad27-f257a658b0e8_스그1.jpg
+			// 2a087772-f261-4b59-ad27-f257a658b0e8_스그1.jpg
+			// '/'를 기즌으로 자른다
+			// 파일 데이터를 완벽하게 삭제처리하면 끝
+			List<NoticeFileVO> noticeFileList = noticeVO.getNoticeFileList();
+			if (noticeFileList != null && noticeFileList.size() > 0) {
+				// 모든 경로 /로 시작하기 때문에 0부터 시작 -> 의미 없음
+				String[] filePath = noticeFileList.get(0).getFileSavepath().split("/");
+				
+				// \resources\notice\{bo_no}... 형태를 알고 있기에 filePath[0] 로 설정
+				String path = filePath[0];
+				deleteFolder(req, path);
+			}
+			
 			result = ServiceResult.OK;
 		} else {
 			result = ServiceResult.FAILED;
 		}
 		return result;
+	}
+	
+	public static void deleteFolder(HttpServletRequest req, String path) {
+		File folder = new File(path);
+		if (folder.exists()) {
+			File[] fileList = folder.listFiles();
+			
+			for(int i=0; i<fileList.length; i++) {
+				if (fileList[i].isFile()) {	// 폴더 안의 있는 파일이 파일일때
+					// 폴더 안에 파일을 차례대로 삭제
+					fileList[i].delete();
+				} else {					// 폴더 안에 있는 파일이 폴더 일때 재귀함수 호출(폴더안으로 들어가라)
+					deleteFolder(req, fileList[i].getPath());
+				}
+			}
+			folder.delete();	// 폴더 삭제(boNo에 해당하는 폴더 삭제)
+		}
 	}
 
 	@Override
@@ -198,7 +262,81 @@ public class NoticeServiceImpl implements INoticeService {
 			}
 		}
 	}
+
+	@Override
+	public NoticeFileVO noticeDownload(int fileNo) {
+		// TODO Auto-generated method stub
+		NoticeFileVO noticeFileVO = noticeMapper.noticeDownload(fileNo);
+		
+		if (noticeFileVO == null) {
+			throw new RuntimeException();
+		}
+		
+		noticeMapper.incrementNoticeDowncount(fileNo); //다운로드 횟수증가
+		
+		return noticeFileVO;
+	}
+
+	@Override
+	public DDITMemberVO selectMember(DDITMemberVO sessionMember) {
+		return profileMapper.selectMember(sessionMember);
+	}
+
+	@Override
+	public ServiceResult profileUpdate(HttpServletRequest req, DDITMemberVO memberVO) {
+		ServiceResult result = null;
+		
+		// 사용자가 수정한 프로필 이미지 정보에 따라서 프로필 이미지 정보 값을 설정 후 memberVO에 셋팅해서 전달한다.
+		String uploadPath = req.getServletContext().getRealPath("/resources/profile");
+		File file = new File(uploadPath);
+		if (!file.exists()) {
+			file.mkdirs();
+		}
+		
+		String proFileImg = "";
+		MultipartFile proFileImgFile = memberVO.getImgFile();
+		if (proFileImgFile.getOriginalFilename() != null && !proFileImgFile.getOriginalFilename().equals("")) {
+			String fileName = UUID.randomUUID().toString();
+			fileName += "_" + proFileImgFile.getOriginalFilename();
+			uploadPath += "/" + fileName;
+			try {
+				proFileImgFile.transferTo(new File(uploadPath));
+			} catch (IllegalStateException | IOException e) {
+				e.printStackTrace();
+			}	// 프로필 이미지 복사
+			proFileImg = "/resources/profile/" + fileName;
+		}
+		
+		// 선택한 프로필 이미지가 존재하지 않으면 "" 공백이 넘어가고
+		// 프로필 이미지가 존재하면 업로드 경로와 파일명으로 구성된 경로가 넘어간다.
+		memberVO.setMemProfileImg(proFileImg);
+		
+		int status = profileMapper.profileUpdate(memberVO);
+		if (status > 0) {
+			result = ServiceResult.OK;
+		} else {
+			result = ServiceResult.FAILED;
+		}
+		return result;
+	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
