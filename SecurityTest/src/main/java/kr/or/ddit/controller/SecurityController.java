49e9edefa03a0d054826556d0ca1c970d969f31b
GuGihyeon <ggh4177@gmail.com>
2023-06-15 11:30:04 +0900

    SecurityController
    로그아웃 처리, JDBC 이용한 인증/인가 처리, UserDetailsService 재정의 중

--- SecurityTest/src/main/java/kr/or/ddit/controller/SecurityController.java
+++ SecurityTest/src/main/java/kr/or/ddit/controller/SecurityController.java
@@ -223,9 +223,100 @@ public class SecurityController {
 	 *   	    넘겨받은 파라미터들 중 authentication안에 principal로 User 정보를 받아서 username과 password를 출력한다.
 	 *   	  (출력 정보는 로그인 성공 시 인증된 회원 정보)
 	 *   
-	 * 			
+	 * 9. 로그아웃 처리
+	 * 로그아웃을 위한 URI를 지정하고, 로그아웃 처리 후에 별도의 작업을 하기 위해서 사용자가 직접 구현한 처리자를 등록할 수 있다.
 	 * 
-	 * 	
+	 * 		환경 설정
+	 * 		- 스프링 시큐리티 설정
+	 * 			> security-context.xml 설정
+	 * 			> <security:logout logout-url="/logout" invalidate-session="true"/>
+	 * 		
+	 * 			** logout 경로는 스프링에서 제공하는 /logout 경로로 설정한다
+	 * 			   logout처리 페이지에서도 action 경로는 /logout으로 설정한다
+	 * 
+	 * 10. JDBC 이용한 인증/인가 처리
+	 * - 지정한 형식으로 테이블을 생성하면 JDBC를 이용해서 인증/인가를 처리할 수 있다.
+	 *   생성한 테이블은 사용자를 관리하는 테이블(users)과 권한을 관리하는 테이블 2개로 구성되어 있다.
+	 *   
+	 *   	데이터베이스 테이블 준비
+	 *   	- users, authorities 테이블 준비
+	 *   
+	 *   	환경 설정
+	 *   	- 의존 라이브러리 설정
+	 *   	> 데이터베이스 관련 라이브러리를 추가한다.
+	 *   	> 기존 데이터베이스 연결을 위한 라이브러리가 추가되어 있다. (SecurityTest 프로젝트 안에는 없기 때문에 새롭게 등록)
+	 *   
+	 *   	스프링 설정
+	 *   	- root-context.xml
+	 *   	> 데이터 소스 설정(기존 설정)
+	 *   
+	 *   	스프링 시큐리티 설정
+	 *   	> security-context.xml 설정
+	 *   	> customPasswordEncoder 빈 등록
+	 *   	> <security:authentication-manager> 태그 내에 설정
+	 *   
+	 *   	비밀번호 암호화 처리기 클래스 정의
+	 *   	- 비밀번호 암호화 처리기
+	 *   	> 스프링 시큐리티 5버전 부터는 기본적으로 PasswordEncoder를 지정해야하는데,
+	 *   	   제대로 하려면 생성된 사용자 테이블(users)에 비밀번호를 암호화하여 저장해야한다.
+	 *   	   테스트를 위해서 생성한 데이터는 암호화를 처리하지 않고 로그인하면  당연히 로그인 에러가 발생할 것이다.(암호화된 비밀번호가 날라가는 게 아니라서)
+	 *   	   그래서 암호화를 하지 않는 PasswordEncoder를 직접 구현하여 지정하면 로그인 시 암호화를 고려하지 않으므로 로그인이 정상적으로 이루어지는 걸 확인할 수 있다.
+	 *   		- security/CustomNoOpPasswordEncoder 클래스 구현
+	 *   
+	 * 11. 사용자 테이블을 이용한 인증/인가 처리
+	 * 스프링 시큐리티가 기본적으로  이용하는 테이블 구조를 그대로 생성해서 사용해도 되지만,
+	 * 기존에 구축된 회원 테이블이 있따면 약간의 작업으로 기존 테이블을 활용할 수 있습니다.
+	 * 		
+	 * 		데이터베이스 테이블 준비
+	 * 		- member, member_auth 테이블 준비
+	 * 		
+	 * 		환경 설정
+	 * 		- 스프링 시큐리티 설정
+	 * 		- security-context.xml 설정
+	 * 		- bcryptPasswordEncoder 빈 등록 진행
+	 * 		- <security:jdbc-user-service> 태그 설정
+	 * 		- <security:password-encoder> 태그 설정
+	 * 
+	 * 		쿼리 정의
+	 * 		- 인증할 때 사용할 쿼리
+	 * 		> select user_id, user_pw, enabled from member where user_id = ?
+	 * 		- 퀀한을 확인할 때 사용할 쿼리
+	 * 		> select m.user_id, ma.auth from member_auth ma, member m where ma.user_no = m.user_no and m.user_id = ?
+	 * 
+	 * 		** BCryptPasswordEncoder 클래스를 이용하여 직접 encode된 비밀번호를 찾아 데이터베이스에 세팅한다.
+	 * 		> 12번에 해당하는 내용에서 자세한 내용으로 확인
+	 * 
+	 * 			BCryptPasswordEncoder 클래스를 활용한 단방향 비밀번호 암호화
+	 * 			- encode() 메서드를 통해서 SHA-2방식의 8바이트 Hash 암호를 매번 램덤하게 생성한다. -> SHA-1 다털림~
+	 * 			- 똑같은 비밀번호를 입력하더라도 암호화되는 문자열을 매번 다른 문자열을 반환한다.
+	 * 
+	 * 		> 비밀번호를 입력하면 암호화된 비밀번호로 인코딩되는데, 암호화된 비밀번호와 디비 테이블에 있는 암호화된 비밀번호가 일치한 지를 파악한 후,
+	 * 		   일치하면 로그인 성공으로 다음 스탭을 진행한다.
+	 * 			- BCryptPasswordEncoder 클래스의 encode() 메서드를 통해 만들어지느 암호화된 해쉬 다이제스트들은 입력한 비밀번호 문자에 해당하는
+	 * 			   수입억개의 다이제스트들 중에서 일치하는 다이제스트가 존재할 경우 비밀번호의 일치를 보고 인증을 성공시켜준다.
+	 * 
+	 * 12. UserDetailsService 재정의
+	 * 스프링 시큐리티의 UserDetailService를 구현하여 사용자 상세 정보를 얻어오는 메서드를 재정의한다.
+	 * 
+	 * 		데이터베이스 테이블
+	 * 		- board 테이블 사용
+	 * 
+	 * 		환경 설정
+	 * 		- 의존 라이브러리 설정(pom.xml)
+	 * 		> 데이터베이스 관련 라이브러리
+	 * 		
+	 * 		스프링 시큐리티 설정
+	 * 		- security-context.xml 설정
+	 * 		> customUserDetailsService 빈 등록
+	 * 		> <security:authentication-provider> 태그 설정
+	 * 
+	 * 		클래스 재정의
+	 * 		- UserDetailsService 재정의
+	 * 		> common.security.CustomUserDetailsService
+	 * 		> 기존 memberMapper에서 사용중인 read를 기반으로 한 readById를 새롭게 재정의하여 사용
+	 * 		> CustomUserDetailsService 클래스 내 loadUserByUsername 메서드에서 인코딩된 비밀번호를 확인해보고 암호화된 비밀번호가 어떻게
+	 * 		    일치하여 인증이되는지를 같이 알아보자
+	 * 
 	 */
 }
 
